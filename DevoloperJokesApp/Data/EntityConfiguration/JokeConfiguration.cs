920e122791aace6f336f69d53991fb0b165e6129
Selemani <lusukama.s@gmail.com>
2025-01-14 14:28:32 +0200

    Update using system for JokeConfiguration

--- DevoloperJokesApp/Data/EntityConfiguration/JokeConfiguration.cs
+++ DevoloperJokesApp/Data/EntityConfiguration/JokeConfiguration.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using ProgramJokesWebApp.Models;
+using DevoloperJokesApp.Models;
 
 namespace DevoloperJokesApp.Data.EntityConfiguration
 {
@@ -29,7 +29,7 @@ namespace DevoloperJokesApp.Data.EntityConfiguration
 
             
             builder.HasQueryFilter(x => !x.IsDeleted);
-    {
+        {
         
     }
 }
--- DevoloperJokesApp/Program.cs
+++ DevoloperJokesApp/Program.cs
@@ -1,7 +1,22 @@
+using Micorosft.AspNetCore.identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.DevoloperJokesApp.Data;
+using Microsoft.DevoloperJokesApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("SQLConnection") ?? throw new InvalidOperationException("Connection string 'SQLConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -14,11 +29,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
+app.UseStaticFiles();
 
+app.UseRouting();
+app.UseAuthorization();
 app.UseAuthorization();
 
-app.MapStaticAssets();
+//app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
